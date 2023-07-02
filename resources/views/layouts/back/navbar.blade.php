998ca30fcce64fc1a21773e7fd15ddf07550c10c
Muhamad Riyanto <98597500+muhamadriyanto21@users.noreply.github.com>
2023-07-02 22:17:04 +0700

    menambahkan fitur terbaru

--- resources/views/layouts/back/navbar.blade.php
+++ resources/views/layouts/back/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-      <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="{{ asset('back/images/logo.svg') }}" class="mr-2" alt="logo"/></a>
+      <a class="navbar-brand brand-logo mr-5" href="index.html">TV Store</a>
       <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('back/images/logo-mini.svg') }}" alt="logo"/></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
@@ -77,7 +77,7 @@
               <i class="ti-settings text-primary"></i>
               Settings
             </a>
-            <a class="dropdown-item">
+            <a class="dropdown-item" href="{{ route('front.login') }}">
               <i class="ti-power-off text-primary"></i>
               Logout
             </a>
