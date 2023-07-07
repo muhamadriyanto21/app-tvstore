9229837bbda91eceb190748a59529b7a848c0ad0
Muhamad Riyanto <98597500+muhamadriyanto21@users.noreply.github.com>
2023-07-07 20:39:59 +0700

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
+        User::create([
+            'name' => 'Admin2',
+            'email' => 'admin2@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
+    }
+}
